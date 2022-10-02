3db87243b9b66d77e6458c5ce5f2578d4a1d4900
kipperzGG <kip@kipperz.gg>
2022-10-02 01:09:08 -0700

    Adding setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+
+# Setting up
+setup(
+    name="vidstream",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<mail@neuralnine.com>",
+    description="sidekip Utilities",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[]
+)
